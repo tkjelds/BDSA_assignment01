20182c8675285084dffef97e683459020beda4d6
tkjelds <tkjelds385@gmail.com>
2022-09-10 16:38:28 +0200

    added rest of c# exercises

--- Assignment1/RegExpr.cs
+++ Assignment1/RegExpr.cs
@@ -1,10 +1,47 @@
-namespace Assignment1;
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
-public static class RegExpr
+namespace Assignment1
 {
-    public static IEnumerable<string> SplitLine(IEnumerable<string> lines) => throw new NotImplementedException();
+    public static class RegExpr
+    {
+        public static IEnumerable<string> SplitLine(IEnumerable<string> lines)
+        {
+            var pattern = @"(\w+)";
+            var rx = new Regex(pattern);
+            foreach (var line in lines)
+            {
+                var match = rx.Match(line);
+                while(match.Success){
+                    yield return match.Value;
+                    match = match.NextMatch();
+                }
+            }
+        }
 
-    public static IEnumerable<(int width, int height)> Resolution(string resolutions) => throw new NotImplementedException();
+        public static IEnumerable<(int width, int height)> Resolution(string resolutions)
+        {
+            var pattern = @"(?<width>\d+)x(?<height>\d+)";
+            var rx = new Regex(pattern);
+            var match = rx.Match(resolutions);
+            while(match.Success){
+                var width = int.Parse(match.Groups["width"].Value);
+                var height = int.Parse(match.Groups["height"].Value);
+                yield return (width,height);
+                match = match.NextMatch();
+            }
+        }
 
-    public static IEnumerable<string> InnerText(string html, string tag) => throw new NotImplementedException();
+        public static IEnumerable<string> InnerText(string html, string tag)
+        {
+            var pattern = @"(<" + tag + @".*?>)(?<string>\w.*?)(</" +tag +">)";
+            var rx = new Regex(pattern);
+            var match = rx.Match(html);
+            while(match.Success){
+                yield return match.Groups["string"].Value;
+                match = match.NextMatch();
+            }
+        }
+    }
 }
